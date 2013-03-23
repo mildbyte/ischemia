8185c2541f5da8c74cbc7ce87254dbbde6e73bcf
Artjoms Iskovs <mildbyte@gmail.com>
2013-03-23 23:16:43 +0000

    Removed some unneeded code.

--- src/ischemia/Ischemia.java
+++ src/ischemia/Ischemia.java
@@ -1,14 +1,9 @@
 package ischemia;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Ischemia {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to Ischemia.");
 		System.out.println("Use Ctrl+C to exit.");
