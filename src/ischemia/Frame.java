f7f8ea5884d9fcd338d7e51fa4032a1bdb8d665b
Artjoms Iskovs <mildbyte@gmail.com>
2013-03-26 18:48:00 +0000

    Turned Frame into a static class that manipulates zipped lists of variables-bindings (environments have to be lists for the metacircular interpreter to work)

--- src/ischemia/Frame.java
+++ src/ischemia/Frame.java
@@ -1,62 +1,66 @@
 package ischemia;
 
-import java.util.HashMap;
-
-//Maps variables to their values
+/**
+ * A static class for manipulating frames.
+ */
 public class Frame {
-	private HashMap<SchemeObject, SchemeObject> mappings;
-	public Frame() {
-		mappings = new HashMap<>();
-	}
-	
+	private Frame() {}
 	
 	/**
 	 * Makes a frame from two lists of variables and values
 	 */
-	public Frame(SchemeObject variables, SchemeObject values) {
-		mappings = new HashMap<>();
-		while (!(variables instanceof EmptyList) && !(values instanceof EmptyList)) {
-			mappings.put(((Pair)variables).car(), ((Pair)values).car());
-			
-			variables = ((Pair)variables).cdr();
-			values = ((Pair)values).cdr();
-		}
+	public static SchemeObject makeFrame(SchemeObject variables, SchemeObject values) {
+		return new Pair(variables, values);
 	}
 	
 	/**
-	 * Looks up the variable in the table, returns null if not found.
+	 * Looks up a variable in the frame, returns null if not found.
 	 */
-	public SchemeObject lookupValue(SchemeObject variable) {
-		return mappings.get(variable);
+	public static SchemeObject lookupValue(SchemeObject frame, SchemeObject variable) {
+		SchemeObject variables = ((Pair)frame).car();
+		SchemeObject values = ((Pair)frame).cdr();
+		
+		while (!(variables instanceof EmptyList) && !(values instanceof EmptyList)) {
+			if (variable.equals(((Pair)variables).car()))
+				return ((Pair)values).car();
+		}
+		
+		return null;
 	}
 	
 	/**
 	 * Sets the variable value even if it doesn't exist.
 	 */
-	public void defineVariable(SchemeObject variable, SchemeObject value) {
-		mappings.put(variable, value);
+	public static void defineVariable(SchemeObject frame, SchemeObject variable, SchemeObject value) {
+		SchemeObject variables = ((Pair)frame).car();
+		SchemeObject values = ((Pair)frame).cdr();
+		
+		while (!(variables instanceof EmptyList) && !(values instanceof EmptyList)) {
+			if (variable.equals(((Pair)variables).car())) {
+				((Pair)values).setCar(value);
+				return;
+			}
+		}	
+		
+		//Add the binding to the frame
+		((Pair)frame).setCar(new Pair(variable, ((Pair)frame).car()));
 	}
 	
 	
 	/**
 	 * Sets the value of a variable if it exists, raises an exception otherwise
 	 */
-	public void setValue(SchemeObject variable, SchemeObject value) throws EvalException {
-		if (!mappings.containsKey(variable)) throw new EvalException("Error: Unbound variable " + variable.print());
+	public static void setValue(SchemeObject frame, SchemeObject variable, SchemeObject value) throws EvalException {
+		SchemeObject variables = ((Pair)frame).car();
+		SchemeObject values = ((Pair)frame).cdr();
 		
-		defineVariable(variable, value);
-	}
-	
-	/**
-	 * Prints all variables bound in the frame
-	 * @return
-	 */
-	public String printAll() {
-		String result = "";
-		for (SchemeObject variable : mappings.keySet()) {
-			result += variable.print() + " ";
-		}
+		while (!(variables instanceof EmptyList) && !(values instanceof EmptyList)) {
+			if (variable.equals(((Pair)variables).car())) {
+				((Pair)values).setCar(value);
+				return;
+			}
+		}	
 		
-		return result;
+		throw new EvalException("Error: Unbound variable " + variable.print());
 	}
 }
